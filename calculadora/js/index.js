926d291be01781ac53329065feebaf9c745a24bf
Herkem <laragom98@gmail.com>
2022-11-22 19:45:59 +0100

    Calculadora parte 3

--- calculadora/js/index.js
+++ calculadora/js/index.js
@@ -1,82 +1,168 @@
 "use strict"
 
-alert("Bienvenido a la calculadora");
+//Declaración del objeto calculadora
+let calculadora = {
+    //Propiedad que almacena el último resultado de la calculadora
+    lastResult: 0,
 
-let valido;
-do{
-    let operador;
-    let operando1;
-    let operando2;
-    do{
-        operador = prompt("Introduce el operador de la operación que quieres realizar");
-        if(!Boolean(operador)){
-            alert("Debes introducir un operador");
-            valido = false;
-        }
-        else{
-            operador = operador.trim();
-            if(operador.length === 1){
-                if(operador === "+" || operador === "-" || operador === "/" || operador === "*"){
-                    valido = true;
-                }
-                else{
-                    alert("Lo siento, el operador no es correcto debe ser '+', '-', '/' o '*'");
-                    valido = false;
-                }
-            }
-            else{
-                alert("Lo siento, el operador solo debe ser un carácter");
-                valido = false;
-            }
-        }
-    }while(!valido);
+    //Método que suma dos operandos
+    suma(operando1, operando2) {
+        this.lastResult = operando1 + operando2;
+        return operando1 + operando2;
+    },
 
-    do{
-        let operandos = prompt("Introduce los operandos separados por un espacio");
+    //Método que resta dos operandos
+    resta(operando1, operando2) {
+        this.lastResult = operando1 - operando2;
+        return operando1 - operando2;
+    },
 
-        if(!Boolean(operandos)){
-            alert("Debes introducir dos operadores");
-            valido = false;
-        }
-        else{
-            operandos = operandos.trim();
-            operando1 = Number(operandos.substring(0, operandos.indexOf(" ")).trim());
-            operando2 = Number(operandos.substring(operandos.indexOf(" ")).trim());
-            if(!Boolean(operando1) || !Boolean(operando2)){
-                alert("Lo siento, los operandos deben de ser dos números");
-                valido = false;
-            }
-            else{
-                valido = true;
-            }
-        }
-    }while(!valido);
+    //Método que multiplica dos operandos
+    multiplicacion(operando1, operando2) {
+        this.lastResult = operando1 * operando2;
+        return operando1 * operando2;
+    },
 
-    switch(operador){
+    //Método que divide dos operandos
+    division(operando1, operando2) {
+        this.lastResult = operando1 / operando2;
+        return operando1 / operando2;
+    }
+};
+
+//Función que pide un operador al usuario y lo devuelve
+function pedirOperador() {
+    let operador
+
+    //Bucle que pide un operador y si no es correcto lo vuelve a pedir
+    do {
+        operador = prompt("Introduce el operador de la operación que quieres realizar. (+, -, * ó /)");
+        if(operador == null){
+            break;
+        }
+        if (!comprobarOperador(operador)) {
+            alert("El operador introducido es incorrecto. Debe ser (+, -, * ó /)")
+        }
+    } while (!comprobarOperador(operador));
+
+    return operador;
+}
+
+//Función que comprueba si el operador que el usuario ha introducido es válido o no
+function comprobarOperador(operador) {
+    let operadores = ['+', '-', '*', '/'];
+
+    if (Boolean(operador)) {
+        operador = operador.trim();
+        return operadores.includes(operador);
+    }
+    return false;
+}
+
+//Función que determina el primer operando de un string
+function pedirPrimerOperando(operandos) {
+    let operando;
+    if (Boolean(operandos)) {
+        operando = operandos.substring(0, operandos.indexOf(" ")).trim();
+
+        //Se comprueba si el número es nuevo o si desea el que ya está almacenado
+        if(operando === 'R'){
+            return calculadora.lastResult;
+        }
+        else if (isFinite(Number(operando))) {
+            return Number(operando);
+        }
+        return -1;
+    }
+    return -1;
+}
+
+//Función que determina el segundo operando de un string
+function pedirSegundoOperando(operandos) {
+    let operando;
+    if (Boolean(operandos)) {
+        operando = operandos.substring(operandos.indexOf(" ")).trim();
+
+        //Se comprueba si el número es nuevo o si desea el que ya está almacenado
+        if(operando === 'R'){
+            return calculadora.lastResult;
+        }
+        else if (isFinite(Number(operando))) {
+            return Number(operando);
+        }
+        return -1;
+    }
+    return -1;
+}
+
+//Función que muestra por pantalla el resultado de la operación seleccionada por el usuario
+function mostrarResultado(operador, operando1, operando2) {
+    //Dependiendo del operador seleccionado se hace una operación distinta
+    switch (operador) {
         case "+":
-            alert(`El resultando ${operando1 + operando2}`);
+            alert(`El resultado ${calculadora.suma(operando1, operando2)}`);
             break;
         case "-":
-            alert(`El resultando ${operando1 - operando2}`);
+            alert(`El resultado ${calculadora.resta(operando1, operando2)}`);
             break;
         case "/":
-            if(operando2 === 0){
-                alert("Lo siento no se puede dividir el ningún número entre 0");
+            //Se comprueba que no se pueda dividir entre 0
+            if (operando2 === 0) {
+                alert("Lo siento no se puede dividir ningún número entre 0");
             }
-            else{
-                alert(`El resultando ${operando1 / operando2}`);
+            else {
+                alert(`El resultado ${calculadora.division(operando1, operando2)}`);
             }
             break;
         case "*":
-            alert(`El resultando ${operando1 * operando2}`);
+            alert(`El resultado ${calculadora.multiplicacion(operando1, operando2)}`);
+    }
+}
+
+let repetir;
+
+//Bucle principal de la ejecución
+outer:do {
+    alert("Bienvenido a la calculadora");
+
+    //Se pide el operador al usuario
+    let operador = pedirOperador();
+
+    //Si el usuario le da a escapa o a cancelar saldrá del bucle totalmente
+    if(operador == null){
+        break outer;
     }
 
+    let operandos;
+    let operando1;
+    let operando2;
+
+    //Bucle que pide los operandos y si no son correctos los vuelve a pedir
+    do {
+        //Se piden los operandos al usuario
+        operandos = prompt("Introduce los operandos separados por un espacio");
+
+        //Si el usuario le da a escapa o a cancelar saldrá del bucle totalmente
+        if(operandos == null){
+            break outer;
+        }
+
+        //Se separa el string de operandos en dos operandos diferentes
+        operando1 = pedirPrimerOperando(operandos);
+        operando2 = pedirSegundoOperando(operandos);
+
+        //Se muestra un mesaje de error si alguno de los operandos está mal introducido
+        if (pedirPrimerOperando(operandos) === -1 || pedirSegundoOperando(operandos) === -1) {
+            alert("Debes introducir dos números separados por un espacio");
+        }
+    } while (pedirPrimerOperando(operandos) === -1 || pedirSegundoOperando(operandos) === -1);
+
+    //Se muestran los resultados de la operación
+    mostrarResultado(operador, operando1, operando2);
+
+    //Se le pregunta al usuario si desea seguir calculando o desea acabar
     let eleccion = prompt("Quieres realizar otra operación S/N?").trim();
-    if(eleccion.toUpperCase() === "S"){
-        valido = true;
-    }
-    else{
-        valido = false;
-    }
-}while(valido);
+    (eleccion.toUpperCase() === "S") ? repetir = true : repetir = false;
+
+} while (repetir);
 
